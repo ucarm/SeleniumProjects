b72c4e8d5b961144f1ea785b09888daf19a52d83
mehmetucar86@gmail.com <mehmetucar86@gmail.com>
2018-06-20 00:08:34 -0500

    Porche Project is completed

--- VideoSearchCount/src/test/java/com/soundPlayer/Youtube.java
+++ VideoSearchCount/src/test/java/com/soundPlayer/Youtube.java
@@ -1,6 +1,7 @@
 package com.soundPlayer;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -13,37 +14,41 @@ import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Youtube {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
+		
 		Faker faker= new Faker();
 		ArrayList<String> results= new ArrayList<String>(); 
-		
+		WebDriver driver;
+//		driver.manage().window().fullscreen();
+//		driver.manage().window().maximize();
+		String result;
 		int i=1;
-		while(i<2) {
-			WebDriver driver = new ChromeDriver();
-			driver.get("https://www.google.com/");
-			String search= faker.music().instrument();
-			driver.findElement(By.cssSelector("input[label='Search']")).sendKeys(search+Keys.ENTER);
-//			StringBuilder pageSource= new StringBuilder(driver.getPageSource());
-//			String searchResult=getHowMany(pageSource);
-			
-			
-			//			if(searchResult.isEmpty())
-//				String searchResult =driver.findElement(By.cssSelector("p[class='num-results first-focus']")).getText();
-//			System.out.println(searchResult);
-//			results.add(i+": "+search+"\t "+searchResult);
+		while(i<10) {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			result="";
+			driver.get("https://www.youtube.com/");
+			Thread.sleep(1000);
+			String search= faker.artist().name();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//div[@id='container']//div[@id='search-input']/input")).sendKeys(search+Keys.ENTER);
+			Thread.sleep(1000);
+			result= driver.findElement(By.xpath("//div[@id='container']//yt-formatted-string[@id='result-count']")).getText();
+			Thread.sleep(1000);
+			System.out.println(search+"\t: "+result);
+			results.add(search+"\t: "+result);
 			i++;
 			driver.close();
 		}
-		System.out.println(results);
+		
+		for (String string : results) {
+			System.out.println(string);
+		}
+		
 	}
 
-//	private static String getHowMany(StringBuilder pageSource) {
-//		int index1=pageSource.indexOf("resultCount");
-//		System.out.println(index1);
-//		int index2= pageSource.indexOf("results\"},\"button\":{\"toggleButtonRenderer\"");
-//		return pageSource.substring(index1+28, index2);
-//	}
+	
 
 }
