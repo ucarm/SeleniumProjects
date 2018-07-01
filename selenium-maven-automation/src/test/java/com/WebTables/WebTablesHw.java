cdc88c971a7ddb3863a13f45288e9bda72bcd1cd
mehmetucar86@gmail.com <mehmetucar86@gmail.com>
2018-06-30 23:40:50 -0500

    file output feature is added

--- selenium-maven-automation/src/test/java/com/WebTables/WebTablesHw.java
+++ selenium-maven-automation/src/test/java/com/WebTables/WebTablesHw.java
@@ -1,8 +1,10 @@
 package com.WebTables;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -44,6 +46,21 @@ public class WebTablesHw {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+
+		// The test output can't be handled by the Jva console. Because it has more than
+		// 2k lines
+		// it outputs a pdf file
+		// output_.pdf file has all the lines command has
+		LocalDateTime now = LocalDateTime.now();
+		// local date time for naming the output file
+		try {
+			PrintStream out = new PrintStream(new FileOutputStream("output_" + now.toString() + "_.txt"));
+			System.setOut(out);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("=========================\nCOMPANY Name\t\t: ****** INC\nTESTING project link\t: "+url+"\nENGINEER\t\t: Mehmet Ucar" );
+		System.out.println("TESTING Started at \t:"+now+"\n=========================\n");
 	}
 
 	@Test
@@ -52,7 +69,6 @@ public class WebTablesHw {
 		// https://forms.zohopublic.com/murodil/report/Applicants/reportperma/DibkrcDh27GWoPQ9krhiTdlSN4_34rKc8ngubKgIMy8
 		// 2) Create a HashMap
 		driver.get(url);
-		Map<Integer, String> allData = new HashMap<>();
 		// 3) change row number to 100, read all data on first page and put uniquID as a
 		Select dropdown = new Select(driver.findElement(By.id("recPerPage")));
 		dropdown.selectByVisibleText("100");
@@ -60,27 +76,26 @@ public class WebTablesHw {
 		int totalNumberofApplication = Integer.parseInt(driver.findElement(By.id("total")).getText());
 		int howManyPages = totalNumberofApplication / 100 + 1;
 		System.out
-				.println("there are " + howManyPages + " pages of records. Total records :" + totalNumberofApplication);
+				.println("There are " + howManyPages + " pages of records.\nTotal records :" + totalNumberofApplication);
 		Map<Integer, String> allResults = new HashMap<>();
 		// loop thru all pages
 		for (int i = 1; i < howManyPages; i++) {
-			Thread.sleep(1000);
+			Thread.sleep(500);
 			saveTableData("reportTab", allResults);
 			// click the next page
 			driver.findElement(By.className("nxtArrow")).click();
 			System.out.println("Page #" + i + " is processed.");
 		}
 		printAllResults(allResults);
-
+		Thread.sleep(1000);
 		int NumberOfEntriesInTheMap = allResults.size();
-		System.out.println("+++++++  RESULT +++++++++");
+		Thread.sleep(1000);
 		assertNotEquals(NumberOfEntriesInTheMap, totalNumberofApplication);
-		System.out.println("Size of the map  \t: " + NumberOfEntriesInTheMap);
-		System.out.println("Number of entries\t: " + totalNumberofApplication);
-		System.out.println("+++++++  +++++ +++++++++");
+		System.out.println("\nSize of the map  \t: " + NumberOfEntriesInTheMap);
+		System.out.println("\nNumber of entries\t: " + totalNumberofApplication+"\n");
 	}
 
-	public void printAllResults(Map allResults) {
+	public void printAllResults(Map<Integer, String> allResults) {
 		Iterator it = allResults.entrySet().iterator();
 		while (it.hasNext()) {
 			Map.Entry pair = (Map.Entry) it.next();
@@ -89,25 +104,7 @@ public class WebTablesHw {
 		}
 	}
 
-	public void printTableData(String id) {
-		String rowXpath = "//table[@id='" + id + "']/tbody/tr";
-		String colXpath = "//table[@id='" + id + "']/tbody/tr[1]/td";
-		int rowsCount = driver.findElements(By.xpath(rowXpath)).size();
-		int colsCount = driver.findElements(By.xpath(colXpath)).size();
-		System.out.println(rowsCount + "x" + colsCount);
-
-		System.out.println("===================");
-		for (int row = 1; row <= rowsCount; row++) {
-			for (int col = 1; col <= colsCount; col++)
-				System.out
-						.print(driver.findElement(By.xpath("(" + rowXpath + ")[" + row + "]/td[" + col + "]")).getText()
-								+ "    \t");
-			System.out.println();
-		}
-		System.out.println("===================");
-	}
-
-	public void saveTableData(String id, Map AllData) {
+	public void saveTableData(String id, Map<Integer, String> allResults) {
 		String rowXpath = "//table[@id='" + id + "']/tbody/tr";
 		String colXpath = "//table[@id='" + id + "']/tbody/tr[1]/td";
 		int rowsCount = driver.findElements(By.xpath(rowXpath)).size();
@@ -125,7 +122,7 @@ public class WebTablesHw {
 				}
 			}
 			// substring to remove the last characher ( - at the end)
-			AllData.put(applicantId, applicantData.substring(0, applicantData.length() - 1));
+			allResults.put(applicantId, applicantData.substring(0, applicantData.length() - 1));
 			// System.out.println(applicantId+"\t"+applicantData);
 
 		}
@@ -133,7 +130,10 @@ public class WebTablesHw {
 
 	@AfterClass
 	public void afterClass() {
-		// driver.close();
+		driver.close();
+		String now= LocalDateTime.now().toString(); 
+		System.out.println("=========================\nTESTING COMPLETED at :"+now);
+
 	}
 
 }
